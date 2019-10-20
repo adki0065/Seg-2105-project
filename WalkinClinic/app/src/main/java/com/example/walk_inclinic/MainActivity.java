8e74327aa44f34c50ec63ec2a42cd2ed06019fba
Noel Khalaf <nkhal049@uottawa.ca>
2019-10-20 14:38:44 -0400

    Create Account Activity

---
+++ WalkinClinic/app/src/main/java/com/example/walk_inclinic/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.walk_inclinic;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnCreateClick(View view){
+
+    }
+
+    public void btnRoleClick(View view){
+
+    }
+}
