8bcf74e54d2fb89ebe82451c3f76b3efebe07346
rkova060 <rkova060@uottawa.ca>
2019-10-20 18:12:17 -0400

    Added logging in

--- app/src/main/java/com/example/segproject/MainActivity.java
+++ app/src/main/java/com/example/segproject/MainActivity.java
@@ -1,55 +1,155 @@
 package com.example.segproject;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivityLog";
 
     private EditText Name;
     private EditText Password;
     private Button Login;
     private Button CreateAccount;
 
+    private FirebaseFirestore db;
+    private CollectionReference accountsRef;
+
+    private MessageDigest digest;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Login = (Button)findViewById(R.id.btnLogin);
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
 
-        Login.setOnClickListener(new View.OnClickListener() {
+        db = FirebaseFirestore.getInstance();
+        accountsRef = db.collection("accounts");
+
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException err) {
+            Log.e(TAG, "Digest error", err);
+        }
+
+//        Login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                validate(Name.getText().toString(), Password.getText().toString());
+//            }
+//        });
+    }
+
+    private void validate(String userName, String userPassword) {
+//        if((userName.equals("Admin")) && (userPassword.equals("5T5ptQ"))){
+//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//            startActivity(intent);
+//        }else if( /* Employee account login */ ){
+//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//            startActivity(intent);
+//        }else if( /* Client account login */ ){
+//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//            startActivity(intent);
+//        }else {
+//            //Login failed try again code
+//        }
+    }
+
+    private void CreateAccount() {
+
+    }
+
+    public void OnLoginPress(View view) {
+        if (digest == null) return;
+
+        String username = Name.getText().toString();
+        final String password = Password.getText().toString();
+
+        if (username == null || username.isEmpty()) {
+            // TODO: Show invalid username
+            return;
+        }
+
+        if (password == null || password.isEmpty()) {
+            // TODO: Show invalid password
+            return;
+        }
+
+        final View activityView = view;
+
+        Query query = accountsRef.whereEqualTo("username", username).limit(1);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot result = task.getResult();
+
+                    if (result.isEmpty()) {
+                        ShowSnackbar(activityView, "Incorrect username or password.");
+                        return;
+                    }
+
+                    for (QueryDocumentSnapshot document : result) {
+                        String checkHash = document.get("password").toString().trim();
+
+                        String hash = Base64.encodeToString(digest.digest(password.getBytes()), Base64.DEFAULT).trim();
+
+                        Log.d(TAG, checkHash);
+                        Log.d(TAG, hash);
+
+                        if (hash.equals(checkHash)) {
+                            // TODO: Remove snackbar and go to next activity
+                            ShowSnackbar(activityView, "Logged in.");
+                        } else {
+                            ShowSnackbar(activityView, "Incorrect username or password.");
+                        }
+
+//                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        break;
+                    }
+                } else {
+                    Log.e(TAG, "Error getting documents: ", task.getException());
+                    ShowSnackbar(activityView, "Error logging in.");
+                }
             }
         });
-    }
 
-    private void validate(String userName, String userPassword){
-        if((userName.equals("Admin")) && (userPassword.equals("5T5ptQ"))){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        }else if( /* Employee account login */ ){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        }else if( /* Client account login */ ){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        }else {
-            //Login failed try again code
-        }
-    }
-    private void CreateAccount(){
 
     }
 
+    private void ShowSnackbar(View view, String text) {
+        Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+                .setAction("CLOSE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                .show();
+    }
 }
